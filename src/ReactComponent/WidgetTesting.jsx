8bb32a5a5eade336aa1d4db29c96d60357801877
zebi823 <zebaa3438@gmail.com>
2026-01-12 18:33:35 +0530

    Initial commit

--- src/ReactComponent/WidgetTesting.jsx
+++ src/ReactComponent/WidgetTesting.jsx
@@ -1,10 +1,13 @@
 import React from "react";
- const Header = () => {
+
+
+ const WidgetTest = () => {
   return (
-    <header>
-      <h1>My Website</h1>
-    </header>
+      <div>
+        <script src="https://callshivai.com/widget2.js?agentId=6937bff1222bfd06ebdf0194&agentName=ShivAI+Assistant&language=Multi-lingual&primaryColor=%234b5563&secondaryColor=%236b7280&accentColor=%23374151&position=bottom-right&chatWidth=380px&chatHeight=320px&autoOpen=false&voiceEnabled=true&companyName=Marketing+Agent&companyDescription=AI-Powered+Support+-+We+offer+24%2F7+voice+support+to+handle+your+business+calls+efficiently+and+professionally.&welcomeMessage=Hi%21+I%27m+Marketing+Agent.+How+can+I+help+you+today%3F&v=1768222759598"></script>
+      </div>    
+    
   );
 };
 
-export default Header;
+export default WidgetTest;
